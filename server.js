824d544589c3388b5f5c05349d9cb7e1b2201310
Gaby0805 <gabbiebrabo@gmail.com>
2025-01-29 21:48:36 -0400

    post funcionando

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 import conn from './db.js';
+import express from 'express'
 
-const resultado = await conn.query("SELECT NOW()");
-console.log("Hora atual do banco:", resultado.rows[0].now);
+const app = express()
+const port = 3220
+
+app.post('/create',  async(req, res) => {
+    try{
+        await conn.query("select insert_usuario('gabriel', 'amorim', 'email', '20579544710', 'bread', 'tp1')")
+        res.status(201).send('funcionando')
+    }
+    catch (err) {
+        console.error(err)
+        res.status(500).send('erro ao criar')
+    }
+})
+
+app.listen(port, () => { 
+    console.log('funcionado')
+})
